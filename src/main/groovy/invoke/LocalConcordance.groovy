2e5b2a9b81aecae5a8e1994f83a8275bd3feb0c2
JonKe <jonkerridge@btinternet.com>
2024-09-28 14:42:24 +0100

    Updated to version 1.0.10

--- src/main/groovy/invoke/LocalConcordance.groovy
+++ src/main/groovy/invoke/LocalConcordance.groovy
@@ -6,7 +6,7 @@ import cluster_cli.run.HostRun
 
 class LocalConcordance {
   static void main(String[] args) {
-    String structureFile = "D:\\IJGradle\\ClusterConcordance\\src\\main\\groovy\\localDSLfiles\\localBible11121"
+    String structureFile = "D:\\IJGradle\\ClusterConcordance\\src\\main\\groovy\\localDSLfiles\\localACM1n1w"
     Class  emitClass = ConcordanceDef
     Class collectClass = ConcordanceCollect
     new HostRun(structureFile, emitClass, collectClass, "Local").invoke()
